f6245f8d3dd7dc76a3b85e06013f242cbc0c0e6a
Mattias Hellman <mattias.hellman@gmail.com>
2026-03-08 12:48:28 +0100

    Change title, move css to root, add workshop pdf

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import "bootstrap-icons/font/bootstrap-icons.css"
-import '/public/style.css'
+import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
