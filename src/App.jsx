92a20090dd3a337862db34ea699a1d82d502d294
Mattias Hellman <mattias.hellman@gmail.com>
2026-03-10 16:11:15 +0100

    Add tasks, users state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Sidebar from './common/Sidebar.jsx'
 import Tasks from './tasks/Tasks.jsx'
 import User from './model/User.js'
+import { useState } from 'react';
 
 const pages = [
   { name: "Dashboard", icon: "bi-grid", href: "#" },
@@ -9,7 +10,7 @@ const pages = [
   { name: "Settings", icon: "bi-gear", href: "#" }
 ];
 
-const users = [
+const usersInit = [
   new User('Mattias Hellman'),
   new User('Sindhuja Parthasarathy'),
   new User('Iffat Zabin'),
@@ -17,14 +18,15 @@ const users = [
   new User('Alexander Haitin')
 ];
 
-const loggedInUser = users[0].name;
-
 function App() {
+  const [users, setUsers] = useState(usersInit);
+  const [loggedIn, setLoggedIn] = useState(users[0].name);
+
   return (
     <>
       <div className='d-flex'>
         <div id='sidebar-container' className='flex-shrink-0'>
-          <Sidebar navlinks={pages} username={loggedInUser}/>
+          <Sidebar navlinks={pages} username={loggedIn}/>
         </div>
         <div className='flex-grow-1'>
           <Tasks assignees={users} />
--- src/tasks/Tasks.jsx
+++ src/tasks/Tasks.jsx
@@ -4,14 +4,17 @@ import Form from './Form';
 import TaskCard from './TaskCard';
 import Searchbar from '../common/Searchbar';
 import TodoItem from '../model/TodoItem';
+import { useState } from 'react';
 
-const todoItems = [
+const tasksInit = [
   new TodoItem('Example Todo 1', 'Description goes here', new Date(2026, 6, 10), 1),
   new TodoItem('Example Todo 1', 'Description goes here', new Date(2026, 6, 10), 1, [{}, {}]),
   new TodoItem('Example Todo 1', 'Description goes here', new Date(2026, 6, 10))
 ];
 
 const Tasks = ({assignees}) => {
+  const [tasks, setTasks] = useState(tasksInit);
+
   return (
     <>
       <Header title='Tasks'>
@@ -19,7 +22,7 @@ const Tasks = ({assignees}) => {
       </Header>
       <div className='container-lg d-flex flex-column justify-content-center'>
         <Form assignees={assignees} />
-        <TaskCard tasks={todoItems} assignees={assignees} />
+        <TaskCard tasks={tasks} assignees={assignees} />
       </div>
     </>
   );
